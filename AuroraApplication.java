1d99b9e6c8dd79b2dc4aa873baf2c193c95f2b29
wangyuxiangnn <wangyuxiangnn@126.com>
2018-11-10 18:40:04 +0800

    springboot

---
+++ AuroraApplication.java
@@ -0,0 +1,12 @@
+package com.example.aurora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuroraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuroraApplication.class, args);
+    }
+}
---
+++ AuroraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.aurora;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AuroraApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
